8ca6140cc6376a62e24c1565f09d074d4e2d977c
William Firmino <williamwf07@gmail.com>
2021-12-31 19:44:11 -0300

    Stylized mobile homepage to follow figma design

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -20,9 +20,8 @@ function App() {
         </div>
       </header>
       <main>
-        <h1>
-          So, you want to travel to <span>Space</span>
-        </h1>
+        <h5>So, you want to travel to</h5>
+        <h1>Space</h1>
         <p>
           Let's face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
