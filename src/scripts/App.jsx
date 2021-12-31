270e8df852acc55fa56ee8bea59383dc02111f87
William Firmino <williamwf07@gmail.com>
2021-12-31 02:30:56 -0300

    Initial Commit

---
+++ src/scripts/App.jsx
@@ -0,0 +1,38 @@
+import "../styles/App.scss";
+
+function App() {
+  return (
+    <div id="App">
+      <header>
+        <a href="#home">
+          <img src="./assets/shared/logo.svg" alt="Logo" />
+        </a>
+
+        <a class="icon">
+          <img src="./assets/shared/icon-hamburger.svg" alt="Hamburger Icon" />
+        </a>
+
+        <div className="links">
+          <a href="#home">00 Home</a>
+          <a href="#destination">01 Destination</a>
+          <a href="#crew">02 Crew</a>
+          <a href="#technology">03 Technology</a>
+        </div>
+      </header>
+      <main>
+        <h1>
+          So, you want to travel to <span>Space</span>
+        </h1>
+        <p>
+          Let's face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we'll give you a truly out of this world
+          experience!
+        </p>
+        <button>Explore</button>
+      </main>
+    </div>
+  );
+}
+
+export default App;
