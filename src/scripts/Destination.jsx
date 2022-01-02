91294f9c6104cca4039d5280c01fbdecaf011bd2
William Firmino <williamwf07@gmail.com>
2022-01-02 01:09:27 -0300

    Added state to change the informations dynamically

--- src/scripts/Destination.jsx
+++ src/scripts/Destination.jsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
 import "../styles/Destination.scss";
 
 function Destination() {
+  const [name, setName] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
+  const [description, setDescription] = useState("");
+  const [distance, setDistance] = useState("");
+  const [travel, setTravel] = useState("");
+
+  useEffect(() => {
+    fetch("../data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setName(data.destinations[0].name);
+        setImgUrl(data.destinations[0].images.png);
+        setDescription(data.destinations[0].description);
+        setDistance(data.destinations[0].distance);
+        setTravel(data.destinations[0].travel);
+      });
+  });
+
   return (
     <main>
       <h2>
         <span>01</span> Pick Your Destination
       </h2>
-      <img src="../assets/destination/image-moon.webp" alt="Moon" />
+      <img src={imgUrl} alt={name} />
       <nav>
         <ul>
           <li>
@@ -23,18 +42,13 @@ function Destination() {
           </li>
         </ul>
       </nav>
-      <h1>Moon</h1>
-      <p>
-        See our planet as you've never seen it before. A perfect relaxing trip
-        away to help regain perspective and come back refreshed. While you're
-        there, take in some history by visiting the Luna 2 and Apollo 11 landing
-        sites.
-      </p>
+      <h1>{name}</h1>
+      <p>{description}</p>
       <div className="separationLine"></div>
       <h3>Avg. Distance</h3>
-      <h4>384,400 KM</h4>
+      <h4>{distance}</h4>
       <h3>Est. Travel Time</h3>
-      <h4>3 Days</h4>
+      <h4>{travel}</h4>
     </main>
   );
 }
