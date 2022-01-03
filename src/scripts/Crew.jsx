eeb284783931bafae5891a4c743639f609909d44
William Firmino <williamwf07@gmail.com>
2022-01-03 03:16:38 -0300

    Added functionality to the dots slider of the crew

--- src/scripts/Crew.jsx
+++ src/scripts/Crew.jsx
@@ -1,32 +1,101 @@
+import { useEffect, useState } from "react";
 import "../styles/Crew.scss";
 
 function Crew() {
+  const [crewMemberIndex, setCrewMemberIndex] = useState(0);
+  const [crewMember, setCrewMember] = useState("douglas hurley");
+
+  const [crewMemberDouglas, setCrewMemberDouglas] = useState(true);
+  const [crewMemberMark, setCrewMemberMark] = useState(false);
+  const [crewMemberVictor, setCrewMemberVictor] = useState(false);
+  const [crewMemberAnousheh, setCrewMemberAnousheh] = useState(false);
+
+  const [name, setName] = useState("");
+  const [imgPngUrl, setImgPngUrl] = useState("");
+  const [imgWebpUrl, setImgWebpUrl] = useState("");
+  const [role, setRole] = useState("");
+  const [bio, setBio] = useState("");
+
+  useEffect(() => {
+    fetch("../data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setCrewMember(data.crew[crewMemberIndex].name.toLowerCase());
+
+        setName(data.crew[crewMemberIndex].name);
+        setImgPngUrl(data.crew[crewMemberIndex].images.png);
+        setImgWebpUrl(data.crew[crewMemberIndex].images.webp);
+        setRole(data.crew[crewMemberIndex].role);
+        setBio(data.crew[crewMemberIndex].bio);
+      });
+  }, [crewMemberIndex]);
+
+  function handleToggle(event) {
+    setCrewMemberIndex(Number(event.target.dataset.index));
+  }
+
+  useEffect(() => {
+    if (crewMember === "douglas hurley") {
+      setCrewMemberDouglas(true);
+      setCrewMemberMark(false);
+      setCrewMemberVictor(false);
+      setCrewMemberAnousheh(false);
+    }
+    if (crewMember === "mark shuttleworth") {
+      setCrewMemberDouglas(false);
+      setCrewMemberMark(true);
+      setCrewMemberVictor(false);
+      setCrewMemberAnousheh(false);
+    }
+    if (crewMember === "victor glover") {
+      setCrewMemberDouglas(false);
+      setCrewMemberMark(false);
+      setCrewMemberVictor(true);
+      setCrewMemberAnousheh(false);
+    }
+    if (crewMember === "anousheh ansari") {
+      setCrewMemberDouglas(false);
+      setCrewMemberMark(false);
+      setCrewMemberVictor(false);
+      setCrewMemberAnousheh(true);
+    }
+  }, [crewMember]);
+
   return (
     <main>
       <h2>
         <span>02</span> Meet Your Crew
       </h2>
       <div className="imgContainer">
-        <img
-          srcset="../assets/crew/image-douglas-hurley.webp"
-          alt="Douglas Hurley's Picture"
-        />
+        <img srcSet={`${imgWebpUrl}, ${imgPngUrl}`} alt={`${name}'s Picture`} />
       </div>
       <nav>
         <ul>
-          <li className="sliderCircle active"></li>
-          <li className="sliderCircle"></li>
-          <li className="sliderCircle"></li>
-          <li className="sliderCircle"></li>
+          <li
+            data-index="0"
+            className={`sliderCircle ${crewMemberDouglas ? "active" : ""}`}
+            onClick={handleToggle}
+          ></li>
+          <li
+            data-index="1"
+            className={`sliderCircle ${crewMemberMark ? "active" : ""}`}
+            onClick={handleToggle}
+          ></li>
+          <li
+            data-index="2"
+            className={`sliderCircle ${crewMemberVictor ? "active" : ""}`}
+            onClick={handleToggle}
+          ></li>
+          <li
+            data-index="3"
+            className={`sliderCircle ${crewMemberAnousheh ? "active" : ""}`}
+            onClick={handleToggle}
+          ></li>
         </ul>
       </nav>
-      <h3>Commander</h3>
-      <h1>Douglas Hurley</h1>
-      <p>
-        Douglas Gerald Hurley is an American engineer, former Marine Corps pilot
-        and former NASA astronaut. He launched into space for the third time as
-        commander of Crew Dragon Demo-2.
-      </p>
+      <h3>{role}</h3>
+      <h1>{name}</h1>
+      <p>{bio}</p>
     </main>
   );
 }
