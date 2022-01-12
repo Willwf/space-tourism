7a47272466d5fb152815521218bbd4a2f735b1fd
William Firmino <williamwf07@gmail.com>
2022-01-12 02:43:33 -0300

    Adjusted Homepage desktop page style

--- src/scripts/Homepage.jsx
+++ src/scripts/Homepage.jsx
@@ -9,14 +9,16 @@ function Homepage({ setActualPage }) {
 
   return (
     <main className="homepage">
-      <h2 className="homepage phrase">So, you want to travel to</h2>
-      <h1 className="homepage title">Space</h1>
-      <p className="homepage description">
-        Let's face it; if you want to go to space, you might as well genuinely
-        go to outer space and not hover kind of on the edge of it. Well sit
-        back, and relax because we'll give you a truly out of this world
-        experience!
-      </p>
+      <div className="texts">
+        <h2 className="homepage phrase">So, you want to travel to</h2>
+        <h1 className="homepage title">Space</h1>
+        <p className="homepage description">
+          Let's face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we'll give you a truly out of this world
+          experience!
+        </p>
+      </div>
       {/* <a href="#">Explore</a> */}
       <Link to="/destination" className="homepage link">
         Explore
