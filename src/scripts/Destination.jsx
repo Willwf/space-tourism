56e4f56950c8e9b72139ae9d9e4512de06724e66
William Firmino <williamwf07@gmail.com>
2022-01-13 00:50:29 -0300

    Adjusted Destination desktop page style

--- src/scripts/Destination.jsx
+++ src/scripts/Destination.jsx
@@ -39,57 +39,61 @@ function Destination({ setActualPage }) {
 
   return (
     <main className="destination">
-      <h2 className="destination phrase">
-        <span>01</span> Pick Your Destination
-      </h2>
-      <img
-        className="destination image"
-        srcSet={`${imgWebpUrl}, ${imgPngUrl}`}
-        alt={name}
-      />
-      <nav className="destination selector">
-        <ul>
-          <li
-            data-index="0"
-            className={destinationIndex === 0 ? "active" : ""}
-            onClick={handleToggle}
-          >
-            <a>Moon</a>
-          </li>
-          <li
-            data-index="1"
-            className={destinationIndex === 1 ? "active" : ""}
-            onClick={handleToggle}
-          >
-            <a>Mars</a>
-          </li>
-          <li
-            data-index="2"
-            className={destinationIndex === 2 ? "active" : ""}
-            onClick={handleToggle}
-          >
-            <a>Europa</a>
-          </li>
-          <li
-            data-index="3"
-            className={destinationIndex === 3 ? "active" : ""}
-            onClick={handleToggle}
-          >
-            <a>Titan</a>
-          </li>
-        </ul>
-      </nav>
-      <h1 className="destination title">{name}</h1>
-      <p className="destination description">{description}</p>
-      <div className="separationLine"></div>
-      <div className="destination info">
-        <div className="destination distance">
-          <h3 className="destination infoTitle">Avg. Distance</h3>
-          <h4 className="destination infoValue">{distance}</h4>
-        </div>
-        <div className="destination travelTime">
-          <h3 className="destination infoTitle">Est. Travel Time</h3>
-          <h4 className="destination infoValue">{travel}</h4>
+      <div>
+        <h2 className="destination phrase">
+          <span>01</span> Pick Your Destination
+        </h2>
+        <img
+          className="destination image"
+          srcSet={`${imgWebpUrl}, ${imgPngUrl}`}
+          alt={name}
+        />
+      </div>
+      <div>
+        <nav className="destination selector">
+          <ul>
+            <li
+              data-index="0"
+              className={destinationIndex === 0 ? "active" : ""}
+              onClick={handleToggle}
+            >
+              <a>Moon</a>
+            </li>
+            <li
+              data-index="1"
+              className={destinationIndex === 1 ? "active" : ""}
+              onClick={handleToggle}
+            >
+              <a>Mars</a>
+            </li>
+            <li
+              data-index="2"
+              className={destinationIndex === 2 ? "active" : ""}
+              onClick={handleToggle}
+            >
+              <a>Europa</a>
+            </li>
+            <li
+              data-index="3"
+              className={destinationIndex === 3 ? "active" : ""}
+              onClick={handleToggle}
+            >
+              <a>Titan</a>
+            </li>
+          </ul>
+        </nav>
+        <h1 className="destination title">{name}</h1>
+        <p className="destination description">{description}</p>
+        <div className="separationLine"></div>
+        <div className="destination info">
+          <div className="destination distance">
+            <h3 className="destination infoTitle">Avg. Distance</h3>
+            <h4 className="destination infoValue">{distance}</h4>
+          </div>
+          <div className="destination travelTime">
+            <h3 className="destination infoTitle">Est. Travel Time</h3>
+            <h4 className="destination infoValue">{travel}</h4>
+          </div>
         </div>
       </div>
     </main>
